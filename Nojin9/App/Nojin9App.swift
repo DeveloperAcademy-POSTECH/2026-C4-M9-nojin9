ac9c0b7ffc0206f0dcedc77b6abc0097b9c911b1
백지훈 <itsjohnbaek@baegjihun-ui-MacBookPro.local>
2026-07-20 20:27:20 +0900

    Add integrated home closet view

--- Nojin9/App/Nojin9App.swift
+++ Nojin9/App/Nojin9App.swift
@@ -6,7 +6,7 @@ struct Nojin9App: App {
 
     var body: some Scene {
         WindowGroup {
-            MyClosetView()
+            HomeView()
                 .environmentObject(store)
         }
     }
